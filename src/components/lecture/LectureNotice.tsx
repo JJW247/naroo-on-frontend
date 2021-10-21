9d32068e6ef67e178cc11d013f4ad3439c6aa0e5
조정우 <adr10won@gmail.com>
2021-10-21 15:31:25 +0900

    lecture notice almost done

--- src/components/lecture/LectureNotice.tsx
+++ src/components/lecture/LectureNotice.tsx
@@ -37,6 +37,7 @@ const LectureNotice: FC<LectureNoticeProps> = ({
   description,
 }) => {
   const [isShowDescription, setIsShowDescription] = useState<boolean>(false);
+  const [isShowEdit, setIsShowEdit] = useState<boolean>(false);
   const onClickDeleteNoticeHandler = async (noticeId: string) => {
     try {
       const response = await axios.delete(
@@ -70,7 +71,7 @@ const LectureNotice: FC<LectureNoticeProps> = ({
           setIsShowDescription(!isShowDescription);
         }}
       >
-        <div className="flex items-center min-h-[41px] max-h-[41px]">
+        <div className="w-full flex items-center min-h-[41px] max-h-[41px]">
           <div className="flex-none min-w-[60px] max-w-[60px] flex justify-center items-center">
             <div className="text-[14px] leading-[150%] text-[#DCDEE2]">
               {array_index}
@@ -90,24 +91,32 @@ const LectureNotice: FC<LectureNoticeProps> = ({
             </div>
           </div>
           <div className="flex-1"></div>
-          {token && userType === 'admin' && (
-            <button
-              className="flex-none max-w-max font-normal text-[12px] leading-[14px] text-[#808695] mr-[8px] mb-[8px]"
-              onClick={() => {
-                onClickDeleteNoticeHandler(id);
-              }}
-            >
-              삭제
-            </button>
-          )}
         </div>
       </div>
       {isShowDescription ? (
-        <div className="min-h-[41px] max-h-[41px] bg-white flex justify-center items-center">
-          <div className="text-[14px] leading-[150%] text-[#515A6E]">
-            {description}
+        <>
+          {token && userType === 'admin' && (
+            <div className="w-full min-h-[41px] max-h-[41px] bg-white flex justify-end items-center">
+              <div className="flex-1"></div>
+              <button className="flex-none rounded-[4px] border-[1px] border-[#EBEEEF] max-w-max font-normal text-[12px] leading-[14px] text-[#808695]">
+                수정
+              </button>
+              <button
+                className="flex-none rounded-[4px] border-[1px] border-[#EBEEEF] max-w-max font-normal text-[12px] leading-[14px] text-[#808695] mx-[8px]"
+                onClick={() => {
+                  onClickDeleteNoticeHandler(id);
+                }}
+              >
+                삭제
+              </button>
+            </div>
+          )}
+          <div className="w-full min-h-[41px] max-h-[41px] bg-white flex justify-center items-center">
+            <div className="text-[14px] leading-[150%] text-[#515A6E]">
+              {description}
+            </div>
           </div>
-        </div>
+        </>
       ) : (
         <></>
       )}
